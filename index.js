491527571570e7cb62e0346b40a2b5a71d38868e
shallihan <smhallihan@gmail.com>
2021-04-06 12:47:25 +0100

    April 6th

---
+++ index.js
@@ -0,0 +1,16 @@
+// Fade-in elements on scroll
+let elementsArray = document.querySelectorAll(".hide");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn();
